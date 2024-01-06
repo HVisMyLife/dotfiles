325f7c435361a88db8bed425a959d3745529e57c
HVisMyLife <HVisMyLife@protonmail.com>
2024-01-06 13:48:46 +0100

    update

--- .config/hypr/hlLauncher.sh
+++ .config/hypr/hlLauncher.sh
@@ -22,6 +22,8 @@ waybar &|
 # Wallpaper
 mode 0 &|
 
+live-server --no-browser /home/hvlife/Documents/Programming/WEB/fluidity 
+
 # Sleep
 #swayidle -w \
 #         timeout 1200 'swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 1' \
